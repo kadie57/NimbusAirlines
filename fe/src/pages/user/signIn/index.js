2a8cc08ecab1470e42a975fae607efbaf5c01ece
KhanhDinh <22025516@vnu.edu.vn>
2024-12-03 18:29:23 +0700

    tao cau truc

---
+++ fe/src/pages/user/signIn/index.js
@@ -0,0 +1,10 @@
+import { memo } from "react";
+const SignIn = () => {
+  return (
+    <>
+      <h1>Sign In</h1>
+    </>
+  );
+};
+
+export default memo(SignIn);
---
+++ fe/src/router.js
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/user/homePage";
+import { ROUTERS } from "./utils/router";
+import MasterLayout from "./pages/user/theme/masterLayout";
+import SignIn from "./pages/user/signIn";
+const renderUserRouter = () => {
+  const userRouters = [
+    {
+      path: ROUTERS.USER.HOME,
+      component: <HomePage />,
+    },
+    {
+      path: ROUTERS.USER.SIGNIN,
+      component: <SignIn/>,
+    },
+  ];
+  return (
+    <MasterLayout>
+      <Routes>
+        {userRouters.map((item, key) => (
+          <Route key={key} path={item.path} element={item.component} />
+        ))}
+      </Routes>
+    </MasterLayout>
+  );
+};
+const RouterCustom = () => {
+  return renderUserRouter();
+};
+
+export default RouterCustom;
